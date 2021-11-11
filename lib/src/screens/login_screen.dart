cd40839b80dc4d85469a2a4166d43f7261427411
Paulo Silva <paulobfsilva@gmail.com>
2021-11-11 21:31:33 +0000

    Login form layout and code

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -10,7 +10,46 @@ class LoginScreenState extends State<LoginScreen> {
   Widget build(context) {
     return Container(
       margin: EdgeInsets.all(20.0),
-      child: Text('Login Screen'),
+      child: Form(
+        child: Column(
+          children: <Widget>[
+            emailField(),
+            passwordField(),
+            Container(margin: EdgeInsets.only(top: 25.0)),
+            submitButton(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget emailField() {
+    return TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        labelText: 'Email Address',
+        hintText: 'you@example.com',
+        icon: Icon(Icons.email),
+      ),
+    );
+  }
+
+  Widget passwordField() {
+    return TextFormField(
+      obscureText: true,
+      decoration: InputDecoration(
+        labelText: 'Password',
+        hintText: '',
+        icon: Icon(Icons.lock),
+      ),
+    );
+  }
+
+  Widget submitButton() {
+    return RaisedButton(
+      color: Colors.blue,
+      child: Text('Login'),
+      onPressed: () {},
     );
   }
 }
